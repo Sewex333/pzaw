3d8e3d8ccb931d32caaa4bebd5c477f7187d3a5f
Sewex333 <168576408+Sewex333@users.noreply.github.com>
2025-01-07 21:30:24 +0100

    Wklejenie kodu z pendrive
    wszystko co zrobiłem podczas pobytu w karpaczu

--- pzaw/projekt/views.py
+++ pzaw/projekt/views.py
@@ -1,4 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Club, Player, Comment
+from django.contrib.auth.decorators import login_required
 
-def home(request):
-    return HttpResponse("Programista lvl 100")
+def club_list(request):
+    clubs = Club.objects.all()
+    return render(request, 'club_list.html', {'clubs': clubs})
+
+def club_detail(request, club_id):
+    club = get_object_or_404(Club, id=club_id)
+    return render(request, 'club_detail.html', {'club': club})
+
+def player_detail(request, player_id):
+    player = get_object_or_404(Player, id=player_id)
+    comments = player.comments.all()
+    return render(request, 'player_detail.html', {'player': player, 'comments': comments})
+
+@login_required
+def add_comment(request, player_id):
+    player = get_object_or_404(Player, id=player_id)
+    if request.method == 'POST':
+        content = request.POST['content']
+        Comment.objects.create(user=request.user, player=player, content=content)
+        return redirect('player_detail', player_id=player.id)
+    return render(request, 'add_comment.html', {'player': player})
