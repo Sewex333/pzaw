1d229a6032dbea0180f37d99d4e44a4e0f826989
Sewex333 <168576408+Sewex333@users.noreply.github.com>
2025-01-10 20:15:13 +0100

    Dodanie Arsenalu, Atletico I realu
    :D

--- pzaw/projekt/views.py
+++ pzaw/projekt/views.py
@@ -12,14 +12,15 @@ def club_detail(request, club_id):
 
 def player_detail(request, player_id):
     player = get_object_or_404(Player, id=player_id)
-    comments = player.comments.all()
+    comments = player.comments.all()  
     return render(request, 'player_detail.html', {'player': player, 'comments': comments})
 
 @login_required
 def add_comment(request, player_id):
     player = get_object_or_404(Player, id=player_id)
     if request.method == 'POST':
-        content = request.POST['content']
-        Comment.objects.create(user=request.user, player=player, content=content)
-        return redirect('player_detail', player_id=player.id)
+        content = request.POST.get('content')
+        if content:
+            Comment.objects.create(user=request.user, player=player, content=content)
+            return redirect('player_detail', player_id=player.id)
     return render(request, 'add_comment.html', {'player': player})
